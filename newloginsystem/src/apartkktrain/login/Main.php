9495f39ac692a356f569cab673c78e6130bd4d75
Apartkktrain <64818647+Apartkktrain@users.noreply.github.com>
2020-07-07 14:29:15 +0900

    Update Main.php

--- newloginsystem/src/apartkktrain/login/Main.php
+++ newloginsystem/src/apartkktrain/login/Main.php
@@ -23,6 +23,7 @@ class Main extends PluginBase implements Listener
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->config = new Config($this->getDataFolder() . "name.yml", Config::YAML);
         $this->config2 = new Config($this->getDataFolder() . "ip.yml", Config::YAML);
+        $this->config3 = new Config($this->getDataFolder() . "uuid.yml", Config::YAML);
 
     }
 
@@ -40,10 +41,12 @@ class Main extends PluginBase implements Listener
     		$player->setImmobile();
     		$player->sendMessage("§a[NEW!LoginSystem]サーバーへようこそ。\nこのサーバーではログインシステムを導入しております。\n/register [password] でアカウントを登録しましょう。");
     	}
-    	if ($this->config->exists($name)) {
+    	if ($this->config->exists($name)) 
+    	{
     		$myip = $this->config2->get($name);
 
-    		if ($myip === $ip) {
+    		if ($myip === $ip) 
+    		{
                 $player->sendMessage("§a[NEWLoginSystem]認証が成功しましたおかえりなさい。");
     		}else{
     			$player->sendMessage("§4[NEWLoginSystem]認証に失敗しました。IPアドレスが変更された可能性がございます。\n/login password でもう一度ログインをお願いいたします。");
@@ -57,7 +60,8 @@ class Main extends PluginBase implements Listener
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool
     {
 
-        if(!$sender instanceof Player){
+        if(!$sender instanceof Player)
+        {
           $sender->sendMessage("§cゲーム内で実行してください。コンソールからは実行ができません。");
           return true;
         }
@@ -68,8 +72,10 @@ class Main extends PluginBase implements Listener
         switch($label){
 
           case 'register':
-         if (!$this->config->exists($name)) {
-         	if (!isset($args[0])) {
+         if (!$this->config->exists($name)) 
+         {
+         	if (!isset($args[0])) 
+         	{
          		$sender->sendMessage("アカウント登録方法：/register [任意のパスワード]");
          	}else{
          		
